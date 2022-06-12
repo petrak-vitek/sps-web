35e385624235aac6e52150bf79af18f5cc9d2bcf
Vít Petrák <76689233+petrak-vitek@users.noreply.github.com>
2022-06-12 23:56:50 +0200

    php

---
+++ main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <title>Vít Petrák</title>
+    <link rel="icon" href="https://sps-web.vejtek.cz/IMG20220511192525.jpg">
+    <link rel="stylesheet" href="style.css">
+
+    <body>
+        <h1>kalkulačka</p></h1>
+        <form action="./test.php" method="get">
+            první číslo: <input type="int" name="a"><br>
+            druhé číslo: <input type="int" name="b"><br>
+            
+          <select name="o" id="o">
+	          <option value="scitani">sčítání</option>
+	         <option value="odcitani" >odčítání</option>
+	         <option value="nasobeni">násobení</option>¨
+             <option value="deleni">dělění</option>
+           </select><br>
+            <input type="submit">
+        </form>
+
+        
+
+
+
+
+    </body>
+</html>
